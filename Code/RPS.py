88e9f45e93ab2bacabff0b42f5e8ca360d76f4ae
Estetic <117092931+Estetic121@users.noreply.github.com>
2023-01-17 14:35:22 +0100

    Rock Paper Scissors
    A very simple and basic game that almost every beginner in python can create. I wrote this program in 1 hour using the residual knowledge in my head. This program is still far from the final version, but I will try to improve it. There are many things you can change in it - interface, create animations, add more features, but right now I don't have enough knowledge for this, so now it's a very simple game.

---
+++ Code/RPS.py
@@ -0,0 +1,59 @@
+import random
+
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+while True:
+
+    selection = input("what do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.: ")
+    if selection == "0":
+        print(rock)
+    elif selection == "1":
+        print(paper)
+    else:
+        print(scissors)
+
+
+
+    options = [rock, paper, scissors]
+    computer_chose = random.choice(options)
+    print("Computer chose:" , computer_chose)
+
+    if selection == "0" and computer_chose == scissors:
+        print("You Win")
+    elif selection == "1" and computer_chose == rock:
+        print("You Win")
+    elif selection == "2" and computer_chose == paper:
+        print("You Win")
+
+    else:
+        print("You Lose")
+
+    qw = input("Press Enter to continue or write a \"no\" to leave me:")
+    if qw == 'no':
+        break
